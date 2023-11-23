565247994e133623896f4b561f9bef8622225dab
LWangllix <vanglikas.lukas@gmail.com>
2023-11-23 22:18:05 +0200

    cleanup

--- src/components/other/Map.tsx
+++ src/components/other/Map.tsx
@@ -1,10 +1,7 @@
-import { useMediaQuery } from "@material-ui/core";
 import { useRef, useState } from "react";
 import styled from "styled-components";
 import { device } from "../../styles";
 import { Url } from "../../utils/texts";
-import Button from "../buttons/Button";
-import Icon from "./Icon";
 import LoaderComponent from "./LoaderComponent";
 
 export interface MapProps {
@@ -15,11 +12,8 @@ export interface MapProps {
 }
 
 const Map = ({ height, value, display }: MapProps) => {
-  const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState(true);
   const iframeRef = useRef<any>(null);
-  const isMobile = useMediaQuery(device.mobileL);
-
 
   const getMapUrl = () => {
     const url = new URL(Url.DRAW);
@@ -28,11 +22,7 @@ const Map = ({ height, value, display }: MapProps) => {
     url.search = params.toString();
     return url.href;
   };
-
   const src = getMapUrl();
-
-
-
   const handleLoadMap = () => {
     setLoading(false);
 
@@ -46,27 +36,12 @@ const Map = ({ height, value, display }: MapProps) => {
     <>
       {loading ? <LoaderComponent /> : null}
       <Container display={display}>
-        {isMobile && (
-          <StyledButton
-            popup
-            type="button"
-            onClick={(e) => {
-              e.preventDefault();
-              setShowModal(!showModal);
-            }}
-          >
-            <StyledIconContainer>
-              <StyledIcon name={"close"} />
-            </StyledIconContainer>
-          </StyledButton>
-        )}
-
         <InnerContainer>
           <StyledIframe
             ref={iframeRef}
             src={src}
             width={"100%"}
-            height={showModal ? "100%" : `${height}`}
+            height={height}
             style={{ border: 0 }}
             allowFullScreen={true}
             onLoad={handleLoadMap}
@@ -85,10 +60,7 @@ const Container = styled.div<{ display: boolean }>`
   display: ${({ display }) => (display ? "flex" : "none")};
 `;
 
-const StyledIcon = styled(Icon)`
-  font-size: 2rem;
-  color: #6b7280;
-`;
+
 
 const InnerContainer = styled.div<{}>`
   position: relative;
@@ -111,32 +83,6 @@ const StyledIframe = styled.iframe<{
   height: ${({ height }) => height};
 `;
 
-const StyledButton = styled(Button)<{ popup: boolean }>`
-  position: absolute;
-  z-index: 10;
-  top: 100px;
-  right: ${({ popup }) => (popup ? 28 : 11)}px;
-  min-width: 28px;
 
-  height: 28px;
-  @media ${device.mobileL} {
-    top: 180px;
-    right: 10px;
-  }
-  button {
-    border-color: #e5e7eb;
-    background-color: white !important;
-    width: 30px;
-    height: 30px;
-    padding: 0;
-    box-shadow: 0px 18px 41px #121a5529;
-  }
-`;
-
-const StyledIconContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 
 export default Map;
