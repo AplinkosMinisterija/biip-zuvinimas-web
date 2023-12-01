e7e6e1ed8a6825badd21573b403e05d9687dba60
LWangllix <vanglikas.lukas@gmail.com>
2023-12-01 16:09:48 +0200

    excel download fix

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,9 @@
-import Axios, { AxiosInstance, AxiosResponse } from "axios";
-import { FishStocking, FishType, Tenant, TenantUser, User } from "./types";
+import Axios, { AxiosInstance, AxiosResponse } from 'axios';
+import { FishStocking, FishType, Tenant, TenantUser, User } from './types';
 
-import { isEmpty } from "lodash";
-import Cookies from "universal-cookie";
-import { Resources } from "./constants";
+import { isEmpty } from 'lodash';
+import Cookies from 'universal-cookie';
+import { Resources } from './constants';
 const cookies = new Cookies();
 
 interface GetAll {
@@ -73,9 +73,12 @@ interface Create {
   id?: string;
 }
 
+const token = cookies.get('token');
+const profileId = cookies.get('profileId');
+
 class Api {
   private AuthApiAxios: AxiosInstance;
-  private readonly proxy: string = "/api";
+  private readonly proxy: string = '/api';
 
   constructor() {
     this.AuthApiAxios = Axios.create();
@@ -85,19 +88,18 @@ class Api {
         if (!config.url) {
           return config;
         }
-        const token = cookies.get("token");
-        const profileId = cookies.get("profileId");
+
         config.url = this.proxy + config.url;
         if (token) {
-          config.headers!.Authorization = "Bearer " + token;
-          if (!isNaN(profileId)) config.headers!["X-Profile"] = profileId;
+          config.headers!.Authorization = 'Bearer ' + token;
+          if (!isNaN(profileId)) config.headers!['X-Profile'] = profileId;
         }
 
         return config;
       },
       (error) => {
         Promise.reject(error);
-      }
+      },
     );
   }
 
@@ -119,7 +121,7 @@ class Api {
     scope,
     geom,
     fields,
-    responseType
+    responseType,
   }: GetAll) => {
     return {
       params: {
@@ -136,22 +138,22 @@ class Api {
         ...(!!query && { query }),
         ...(!!scope && { scope }),
         ...(!!fields && { fields }),
-        ...(!!responseType && { responseType })
-      }
+        ...(!!responseType && { responseType }),
+      },
     };
   };
 
   getAll = async ({ resource, id, ...rest }: GetAll) => {
     const config = this.getCommonConfigs(rest);
     return this.errorWrapper(() =>
-      this.AuthApiAxios.get(`/${resource}${id ? `/${id}` : ""}/all`, config)
+      this.AuthApiAxios.get(`/${resource}${id ? `/${id}` : ''}/all`, config),
     );
   };
 
   get = async ({ resource, id, ...rest }: GetAll) => {
     const config = this.getCommonConfigs(rest);
     return this.errorWrapper(() =>
-      this.AuthApiAxios.get(`/${resource}${id ? `/${id}` : ""}`, config)
+      this.AuthApiAxios.get(`/${resource}${id ? `/${id}` : ''}`, config),
     );
   };
 
@@ -159,281 +161,252 @@ class Api {
     const config = {
       params: {
         ...(!!populate && { populate }),
-        ...(!!scope && { scope })
-      }
+        ...(!!scope && { scope }),
+      },
     };
 
     return this.errorWrapper(() =>
-      this.AuthApiAxios.get(`/${resource}${id ? `/${id}` : ""}`, config)
+      this.AuthApiAxios.get(`/${resource}${id ? `/${id}` : ''}`, config),
     );
   };
 
   update = async ({ resource, id, params }: UpdateOne) => {
     return this.errorWrapper(() =>
-      this.AuthApiAxios.patch(`/${resource}/${id ? `/${id}` : ""}`, params)
+      this.AuthApiAxios.patch(`/${resource}/${id ? `/${id}` : ''}`, params),
     );
   };
 
   delete = async ({ resource, id }: Delete) => {
-    return this.errorWrapper(() =>
-      this.AuthApiAxios.delete(`/${resource}/${id}`)
-    );
+    return this.errorWrapper(() => this.AuthApiAxios.delete(`/${resource}/${id}`));
   };
   create = async ({ resource, id, params }: Create) => {
     return this.errorWrapper(() =>
-      this.AuthApiAxios.post(`/${resource}${id ? `/${id}` : ""}`, params)
+      this.AuthApiAxios.post(`/${resource}${id ? `/${id}` : ''}`, params),
     );
   };
 
   checkAuth = async (): Promise<User> => {
-    return this.errorWrapper(() => this.AuthApiAxios.get("/auth/me"));
+    return this.errorWrapper(() => this.AuthApiAxios.get('/auth/me'));
   };
 
   logout = async () => {
-    return this.errorWrapper(() =>
-      this.AuthApiAxios.post("/auth/users/logout")
-    );
+    return this.errorWrapper(() => this.AuthApiAxios.post('/auth/users/logout'));
   };
 
   authApi = async ({ resource, params }: AuthApiProps) => {
-    return this.errorWrapper(() =>
-      this.AuthApiAxios.post(`/${resource}`, params || {})
-    );
+    return this.errorWrapper(() => this.AuthApiAxios.post(`/${resource}`, params || {}));
   };
 
   refreshToken = async () => {
     return this.authApi({
       resource: Resources.REFRESH_TOKEN,
-      params: { token: cookies.get("refreshToken") }
+      params: { token: cookies.get('refreshToken') },
     });
   };
 
   login = async (params: any) => {
     return this.authApi({
       resource: Resources.LOGIN,
-      params
+      params,
     });
   };
 
   eGatesSign = async () => {
     return this.authApi({
-      resource: Resources.E_GATES_SIGN
+      resource: Resources.E_GATES_SIGN,
     });
   };
 
   eGatesLogin = async (params: any) => {
     return this.authApi({
       resource: Resources.E_GATES_LOGIN,
-      params
+      params,
     });
   };
 
-  tenantUsers = async ({
-    page
-  }: TableList): Promise<GetAllResponse<TenantUser>> =>
+  tenantUsers = async ({ page }: TableList): Promise<GetAllResponse<TenantUser>> =>
     await this.get({
       resource: Resources.TENANT_USERS,
       populate: [Resources.USER],
       page,
-      pageSize: "12"
+      pageSize: '12',
     });
 
   createTenantUser = async (params: any): Promise<User> => {
     return await this.create({
       resource: Resources.INVITE_USER,
-      params
+      params,
     });
   };
   updateTenantUser = async (params: any, id?: string): Promise<User> => {
     return await this.update({
       resource: Resources.TENANT_USERS,
       params,
-      id
+      id,
     });
   };
 
   deleteTenantUser = async (id: string): Promise<User> =>
     await this.delete({
       resource: Resources.TENANT_USERS,
-      id
+      id,
     });
 
   updateProfile = async (id: string, params: any): Promise<User> =>
     await this.update({
       resource: Resources.USERS,
       params,
-      id
+      id,
     });
 
   updateMyProfile = async (params: any): Promise<User> =>
     await this.update({
       resource: Resources.ME,
-      params
+      params,
     });
 
   getFishTypes = async (): Promise<GetAllResponse<FishType>> =>
     await this.get({
       resource: Resources.FISH_TYPES,
-      pageSize: "999"
+      pageSize: '999',
     });
 
   getMunicipalities = async (): Promise<any> =>
     await this.get({
       resource: Resources.MUNICIPALITIES,
-      pageSize: "999"
+      pageSize: '999',
     });
 
   getFishAges = async (): Promise<GetAllResponse<any>> =>
     await this.get({
       resource: Resources.FISH_AGES,
-      pageSize: "999"
+      pageSize: '999',
     });
 
-  getLocations = async ({
-    search = "",
-    page,
-    geom
-  }: TableList): Promise<any[]> =>
+  getLocations = async ({ search = '', page, geom }: TableList): Promise<any[]> =>
     await this.get({
       resource: Resources.LOCATION,
       search,
       geom,
-      page
+      page,
     });
   geUsersByTenant = async (): Promise<User[]> =>
     await this.getAll({
-      resource: Resources.USERS
+      resource: Resources.USERS,
     });
 
   geSignatureUsers = async (
-    municipalityId: string
+    municipalityId: string,
   ): Promise<{ id: string; name: string; users: string[] }[]> =>
     await this.get({
       resource: Resources.SIGNATURE_USERS,
-      municipalityId
+      municipalityId,
     });
 
   getTenants = async ({ filter, page }: TableList): Promise<Tenant> =>
     await this.get({
       resource: Resources.TENANTS,
       filter,
-      page
+      page,
     });
 
-  getFishStockings = async ({
-    filter,
-    page
-  }: TableList): Promise<GetAllResponse<FishStocking>> =>
+  getFishStockings = async ({ filter, page }: TableList): Promise<GetAllResponse<FishStocking>> =>
     await this.get({
       resource: Resources.FISH_STOCKING,
-      populate: [
-        "batches",
-        "createdBy",
-        "reviewedBy",
-        "assignedTo",
-        "status",
-        "location"
-      ],
+      populate: ['batches', 'createdBy', 'reviewedBy', 'assignedTo', 'status', 'location'],
       filter,
       page,
-      pageSize: "12"
+      pageSize: '12',
     });
 
   getFishStocking = async (id: string): Promise<FishStocking> =>
     await this.getOne({
       resource: Resources.FISH_STOCKING,
       populate: [
-        "batches",
-        "reviewedBy",
-        "createdBy",
-        "assignedTo",
-        "status",
-        "location",
-        "geom",
-        "stockingCustomer",
-        "images",
-        "fishOriginReservoir",
-        "assignedToInspector"
+        'batches',
+        'reviewedBy',
+        'createdBy',
+        'assignedTo',
+        'status',
+        'location',
+        'geom',
+        'stockingCustomer',
+        'images',
+        'fishOriginReservoir',
+        'assignedToInspector',
       ],
-      id
+      id,
     });
 
   registerFishStocking = async (params: any): Promise<FishStocking> =>
     await this.create({
       resource: Resources.FISH_STOCKING_REGISTER,
-      params
+      params,
     });
 
   updateFishStocking = async (params: any, id: string): Promise<FishStocking> =>
     await this.update({
       resource: Resources.FISH_STOCKING_REGISTER,
       params,
-      id
+      id,
     });
 
   deleteFishStocking = async (id: string): Promise<FishStocking> =>
     await this.delete({
       resource: Resources.FISH_STOCKING,
-      id
+      id,
     });
 
   cancelFishStocking = async (id: string): Promise<FishStocking> =>
     await this.update({
       resource: Resources.FISH_STOCKING_CANCEL,
-      id
+      id,
     });
 
   reviewFishStocking = async (params: any): Promise<FishStocking> =>
     await this.create({
       resource: Resources.FISH_STOCKING_REVIEW,
-      params
+      params,
     });
   getSettings = async (): Promise<any> =>
     await this.get({
-      resource: Resources.SETTINGS
+      resource: Resources.SETTINGS,
     });
 
   getRecentLocations = async (): Promise<any> =>
     await this.get({
-      resource: Resources.RECENT_LOCATIONS
+      resource: Resources.RECENT_LOCATIONS,
     });
 
   deletePhoto = async (id: string): Promise<any> =>
     await this.delete({
       resource: Resources.PHOTOS,
-      id
+      id,
     });
 
-  uploadFiles = async (
-    fishStockingId: any,
-    files: File[] = []
-  ): Promise<any> => {
+  uploadFiles = async (fishStockingId: any, files: File[] = []): Promise<any> => {
     if (isEmpty(files)) return [];
 
     const config = {
-      headers: { "Content-Type": "multipart/form-data" }
+      headers: { 'Content-Type': 'multipart/form-data' },
     };
 
     try {
       const data = await Promise.all(
         files?.map(async (file) => {
           const formData = new FormData();
-          formData.append("fishStocking", fishStockingId);
-          formData.append("file", file);
-          const { data } = await this.AuthApiAxios.post(
-            `/${Resources.PHOTOS}`,
-            formData,
-            config
-          );
+          formData.append('fishStocking', fishStockingId);
+          formData.append('file', file);
+          const { data } = await this.AuthApiAxios.post(`/${Resources.PHOTOS}`, formData, config);
           return data;
-        })
+        }),
       );
 
       return data?.flat()?.map((file) => {
         return {
           id: file.id,
           name: file.name,
-          url: file?.url
+          url: file?.url,
         };
       });
     } catch (e: any) {
@@ -441,13 +414,23 @@ class Api {
     }
   };
 
-  getExcel = async ({ filter, pageSize }: TableList): Promise<any> =>
-    await this.get({
-      resource: Resources.EXCEL,
-      filter,
-      pageSize,
-      responseType: "blob"
-    });
+  getExcel = async ({ filter }: TableList): Promise<any> => {
+    const response = await fetch(
+      `${this.proxy}/${Resources.EXCEL}?filter=${JSON.stringify(filter)}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+          ...(profileId && !isNaN(profileId) && { 'X-Profile': profileId }),
+          body: JSON.stringify(filter),
+        },
+      },
+    );
+
+    const data = await response.blob();
+
+    return data;
+  };
 }
 
 const api = new Api();
