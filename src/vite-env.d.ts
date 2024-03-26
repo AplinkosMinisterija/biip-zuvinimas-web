be6e1e74210a9ef65efba158d2a93842b4a35195
Dovile <dovile.maminskaite@am.lt>
2024-03-26 14:02:32 +0200

    delete unused arg

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,6 @@
 
 interface ImportMetaEnv {
   readonly NODE_ENV: 'development' | 'production';
-  readonly VITE_ZUVINIMAS_API_BASE_URL?: string;
   readonly VITE_SENTRY_DSN?: string;
   readonly VITE_ENVIRONMENT?: string;
   readonly VITE_MAPS_HOST?: string;
